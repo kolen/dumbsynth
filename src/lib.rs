39315b6acf9967f82022afce8f6e3ed9a1966cff
kolen <incredible.angst@gmail.com>
2020-10-18 02:09:07 +0300

    Generate sawtooth wave non-optimized way

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 use dasp::signal::Signal;
 
+const SAMP_RATE: i32 = 44_100;
+
 pub struct Saw {
     frequency: f32,
     phase: i32,
@@ -18,9 +20,23 @@ impl Signal for Saw {
     type Frame = i16;
 
     fn next(&mut self) -> i16 {
-        let value = ((std::f32::consts::TAU * (self.phase as f32) * self.frequency)
-                     / 44_100f32)
-            .sin();
+        let mut value = 0f32;
+
+        let mut harm_f = self.frequency;
+        let mut harm_i = 1;
+
+        while harm_f < ((SAMP_RATE as f32) / 2f32) {
+            let polarity = if harm_i % 2 == 0 { 1f32 } else { -1f32 };
+            value += polarity
+                * (((std::f32::consts::TAU * (self.phase as f32) * harm_f) / (SAMP_RATE as f32))
+                    .sin())
+                / harm_i as f32;
+
+            harm_i += 1;
+            harm_f = self.frequency * (harm_i as f32);
+        }
+
+        value = -(std::f32::consts::FRAC_1_PI * value);
 
         self.phase += 1;
         (value * 32768f32) as i16
